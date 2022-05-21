0c356db8b88427ab17cb2e2827711ddb94d0e26d
TricoMint <11ldannen@gmail.com>
2022-05-21 00:25:22 -0700

    Update index.html script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("bubbles");
+
 
 /*Rock Paper Scissors 
 Create function with 3 different values
@@ -7,7 +7,12 @@ Create function with 3 different values
 1 is scissors
 
 */
+    let win = 0;
+    let loss = 0;
+    let tie = 0;
 
+
+for (let i = 0; i < 5; i++ ) { 
 
 function computerPlay() {
 
@@ -24,26 +29,59 @@ const playerPrompt = prompt("Rock, Paper, or Scissors?");
 
 const playerSelection = playerPrompt.toLowerCase();
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "rock")
-    console.log("Rock and rock? A tie.");  
-    else if (playerSelection === "rock" && computerSelection === "paper")
-    console.log("Paper beats Rock... you lose... "); 
-    else if (playerSelection === "rock" && computerSelection === "scissors")
-    console.log("Rock beats Scissors, you win!");
-    else if (playerSelection === "paper" && computerSelection === "rock")
-    console.log("Paper beats rock! You win!");  
-    else if (playerSelection === "paper" && computerSelection === "paper")
-    console.log("Paper vs paper is just more paper. A tie."); 
-    else if (playerSelection === "paper" && computerSelection === "scissors")
-    console.log("Paper gets cut up by scissors, you lose.");
-    else if (playerSelection === "scissors" && computerSelection === "rock")
-    console.log("Scissors loses to rock.");  
-    else if (playerSelection === "scissors" && computerSelection === "paper")
-    console.log("scissors beats paper."); 
-    else if (playerSelection === "scissors" && computerSelection === "scissors")
-    console.log("Scissors vs scissors is a stalemate. A tie");
-    else (console.log("That's not how you play this game.")) }
-    
-    function game()
+ 
+    function playRound(playerSelection, computerSelection) {
+        
 
+
+        if (playerSelection === "rock" && computerSelection === "rock") {
+            tie = ++tie; 
+           return "Rock and rock? A tie." +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties"; }
+        else if (playerSelection === "rock" && computerSelection === "paper") {
+            loss = ++loss; 
+            return "Paper beats Rock... you lose... " +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";} 
+        else if (playerSelection === "rock" && computerSelection === "scissors") {
+            win = ++win; 
+            return "Rock beats Scissors, you win!" + 
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";}
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            win = ++win;
+            return "Paper beats rock! You win!" +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";}  
+        else if (playerSelection === "paper" && computerSelection === "paper") {
+            tie = ++tie;
+            return "Paper vs paper is just more paper. A tie." +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";} 
+        else if (playerSelection === "paper" && computerSelection === "scissors") {
+            loss = ++loss;
+            return "Paper gets cut up by scissors, you lose." +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";}
+        else if (playerSelection === "scissors" && computerSelection === "rock") {
+            loss = ++loss;
+            return "Scissors loses to rock. You lose." +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";}  
+        else if (playerSelection === "scissors" && computerSelection === "paper") {
+            win = ++win;
+            return "scissors beats paper. You win." + 
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";} 
+        else if (playerSelection === "scissors" && computerSelection === "scissors") {
+            tie = ++tie;
+            return "Scissors vs scissors is a stalemate. A tie." +
+            "\nGame stats: " + win + " wins, " + loss + " losses, " + tie + " ties";}
+        else return "That's not how you play this game.";
+} 
+   
+console.log(playRound(playerSelection, computerSelection)); 
+
+
+
+    }   
+
+if (win > loss) {
+    console.log("You win overall!")}
+else if (win < loss) {
+    console.log("You lost overall...")}
+else if (tie = 5) {console.log("How in tarnation did you manage that?")}
+else {console.log("Yea something went wrong on my end.")}
